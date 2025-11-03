e2ff2b042f76f8eee1167296ca00c4b40ba4245d
Morten Munk <mortenmunk97@gmail.com>
2025-11-03 22:14:22 +0100

    graph

--- src/graph.py
+++ src/graph.py
@@ -8,7 +8,30 @@ class Graph:
         self._connect_nodes()
 
     def _connect_nodes(self):
-        pass
+        if len(self.nodes) < 2:
+            return
+
+        match self.topology:
+            case "mesh":
+                self._random_mesh()
+            case "ring":
+                nodes = len(self.nodes)
+                for i in range(nodes):
+                    a = self.nodes[i]
+                    b = self.nodes[i + 1 % nodes]
+                    self._connect(a, b)
+            case "star":
+                center = self.nodes[0]
+                for node in self.nodes[1:]:
+                    self._connect(center, node)
+            case "full":
+                for i, a in enumerate(self.nodes):
+                    for b in self.nodes[i + 1 :]:
+                        self._connect(a, b)
+            case _:
+                ValueError(f"Topology: {self.topology} is not valid")
+
+    # TODO: def _random_mesh()
 
     def _connect(self, a: Node, b: Node):
         a.neighbors.add(b)
